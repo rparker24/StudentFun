05e5ae696702b9a990f14ee47470d8b43a4cb67c
rparker24 <robert@robertmparker.net>
2016-04-22 18:21:47 -0400

    progress?

--- main.js
+++ main.js
@@ -2,19 +2,17 @@ var prompt = require('prompt');
 var Student = require('./student.js');
 var Bus = require('./bus.js');
 
+var schoolBus = new Bus('Jerry', 'orange', 100);
+
 prompt.start();
 prompt.get(['name', 'gpa', 'detentions', 'catchPhrase'], function(err, result) {
 	var student1 = new Student(result.name, result.gpa, result.detentions, result.catchPhrase);
 	// console.log(JSON.stringify(student1));
-	student1.canStudentHaveFun();
+	schoolBus.studentEntersBus(student1);
 });
-
-var schoolBus = new Bus('Jerry', 'orange', 100);
-
-schoolBus.studentEntersBus(student1);
 
 var studentList = [];
 
-var student2 = new Student('Dave', , 9, 3)
+// var student2 = new Student('Dave', , 9, 3)
 
 schoolBus.busChatter();
